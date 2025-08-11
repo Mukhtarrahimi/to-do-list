3240618a05dfbf8f11c309c725c003f1788e38d3
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-11 09:04:50 +0430

    Add list_box.

--- to_do_list.py
+++ to_do_list.py
@@ -37,6 +37,12 @@ task_entry.focus()
 button = Button(frame, text="ADD", font="arial 20 bold", width=6, bg="#5a95ff", fg="#fff", bd=0)
 button.place(x=300, y=0)
 
+# listbox
+frame1 = Frame(root, bd=3, width=700, height=280, bg="#32405b")
+frame1.pack(pady=(160, 0))
+
+list_box = Listbox(frame1, font=("arial", 12), width=40, height=16, bg="#32405b", fg="white", cursor="hand2", selectbackground="5a95ff")
+list_box.pack(side=LEFT, fill=BOTH, padx=2)
 
 
 
