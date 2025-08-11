2544c7b00b32b5d20e834bba99fbdc6af5b67cc6
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-11 08:03:25 +0430

    Add main frame and task entry.

--- to_do_list.py
+++ to_do_list.py
@@ -24,4 +24,14 @@ Label(root, image=note_image).place(x=340, y=25)
  
 heading = Label(root, text="ALL TASK", font="arial 20 bold", fg="white", bg="#32405b")
 heading.place(x=130, y=20)
+
+# main
+frame = Frame(root, width=400, height=50, bg="white")
+frame.place(x = 0, y = 180)
+
+task = StringVar()
+task_entry = Entry(frame, width=18, font="arial 20", bd=0)
+task_entry.place(x=10, y=7)
+
+
 root.mainloop()  
